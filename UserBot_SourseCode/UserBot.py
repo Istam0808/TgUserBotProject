b77f2342873b179c763842f1d0a93378263fe1bf
Istam0808 <mamadaliyevistam@gmail.com>
2023-10-30 23:58:53 +0500

    Update (add command /bust)

--- UserBot_SourseCode/UserBot.py
+++ UserBot_SourseCode/UserBot.py
@@ -4,49 +4,61 @@ from time import sleep
 import random
 
 app = Client("my_account")
-api_id = "tour_api_id"
-api_hash = "your ip hash"
+api_id = "your_api_id"
+api_hash = "your_api_hash"
 
-# Команда type
-@app.on_message(filters.command("type", prefixes=".") & filters.me)
-def type(_, msg):
-    orig_text = msg.text.split(".type ", maxsplit=1)[1]
-    text = orig_text
-    tbp = "" # to be printed
-    typing_symbol = "▒"
- 
-    while(tbp != orig_text):
-        try:
-            msg.edit(tbp + typing_symbol)
-            sleep(0.05) # 50 ms
- 
-            tbp = tbp + text[0]
-            text = text[1:]
- 
-            msg.edit(tbp)
-            sleep(0.05)
- 
-        except FloodWait as e:
-            sleep(e.x)
+# Команда bust (пишем  /bust) 
 
-# Команда взлома пентагона
-@app.on_message(filters.command("hack", prefixes=".") & filters.me)
-def hack(_, msg):
-    perc = 0
+# @app.on_message(filters.command("bust"))
+# def bust(client, message):
+#     for i in range(1000):
+#         app.send_message(message.chat.id, "SHAXA LOX")
+# app.run()
+
+
+# Команда type (пишем .type)
+
+# @app.on_message(filters.command("type", prefixes=".") & filters.me)
+# def type(_, msg):
+#     orig_text = msg.text.split(".type ", maxsplit=1)[1]
+#     text = orig_text
+#     tbp = "" # to be printed
+#     typing_symbol = "▒"
  
-    while(perc < 100):
-        try:
-            text = "👮‍ Взлом вашего телефона в процессе ..." + str(perc) + "%"
-            msg.edit(text)
+#     while(tbp != orig_text):
+#         try:
+#             msg.edit(tbp + typing_symbol)
+#             sleep(0.05) # 50 ms
  
-            perc += random.randint(1, 3)
-            sleep(0.1)
+#             tbp = tbp + text[0]
+#             text = text[1:]
  
-        except FloodWait as e:
-            sleep(e.x)
+#             msg.edit(tbp)
+#             sleep(0.05)
  
-    msg.edit("🟢 Ваш телефон успешно взломан!")
-    sleep(e.x)
+#         except FloodWait as e:
+#             sleep(e.x)
+# app.run()
+
+
+# Команда взлома пентагона (пишем .hack)
+# @app.on_message(filters.command("hack", prefixes=".") & filters.me)
+# def hack(_, msg):
+    # perc = 0
+ 
+    # while(perc < 100):
+    #     try:
+    #         text = "👮‍ Взлом вашего телефона в процессе ..." + str(perc) + "%"
+    #         msg.edit(text)
+ 
+    #         perc += random.randint(1, 3)
+    #         sleep(0.1)
+ 
+    #     except FloodWait as e:
+    #         sleep(e.x)
+ 
+    # msg.edit("🟢 Ваш телефон успешно взломан!")
+    # sleep(e.x)
     
     # sleep(3)
     # msg.edit("👽 Поиск секретных данных о вас ...")
@@ -64,6 +76,4 @@ def hack(_, msg):
     #         sleep(e.x)
  
     # msg.edit("🦖 Найдены данные о существовании вас на земле!")
-
-app.run()
-
+# app.run()
